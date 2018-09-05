d26a14fec9e58a41a6b7eadffb3aa9490a21c553
Minamiciccc <june28munazo@gmail.com>
2018-09-05 08:18:20 -0700

    modified

--- Lab1.m
+++ Lab1.m
@@ -11,44 +11,65 @@ NSString *getUserInput(NSString *prompt){
     return [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
 
-void *showOptions(){
+void *selectOptions(){
+    NSInteger infinity = 1;
+    while(infinity){
+        printf("[1] Uppercase\n");
+        printf("[2] Lowercase\n");
+        printf("[3] Numberize\n");
+        printf("[4] Canadianize\n");
+        printf("[5] Respond\n");
+        printf("[6] De-Space-It\n");
+        printf("[0] Quit\n");
+        NSString *opt = getUserInput(@"Choose and enter the number of option: ");
+        NSInteger num = [opt integerValue];
+        NSString *input;
+        switch(num){
+            case 0:
+                infinity = 0;
+                break;
+            case 1:
+                input = getUserInput(@"Enter your name:");
+                NSLog(@"%@", [input uppercaseString]);
+                break;
+            case 2:
+                input = getUserInput(@"Enter your name:");
+                NSLog(@"%@", [input lowercaseString]);
+                break;
+            case 3:
+                input = getUserInput(@"Enter numbers:");
+                NSInteger inputNSInt = [input integerValue];
+                NSLog(@"%d",inputNSInt);
+                break;
+            case 4:
+                input = getUserInput(@"Enter something on your mind:");
+                NSString *canadianized = [input stringByAppendingString:@",eh?"];
+                [NSString stringWithFormat:@"%@/%@",input,@",eh?"];
+                NSLog(@"%@", canadianized);
+                break;            
+            case 5:
+                input = getUserInput(@"Enter any question:");
+                NSString *last = [input substringFromIndex:[input length]-1];
+                if([last isEqualToString:@"?"]){
+                    NSLog(@"%@",@"I don't know");
+                }else if([last isEqualToString:@"!"]){
+                    NSLog(@"%@",@"Whoa, calm down!");
+                }
+                break;
+            case 6:
+                input = getUserInput(@"Enter something on your mind:");
+                NSString *space = [input stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+                NSLog(@"%@",space);
+                break;
+        }
+    }
+
 }
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-        // insert code here...
-        NSString *input = getUserInput(@"Enter your name:");
-        //uppercase
-        NSLog(@"%@", [input uppercaseString]);
-        
-        //lowercase
-        NSLog(@"%@", [input lowercaseString]);
-        
-        //Num
-        // int inputInt = [input intValue];
-        NSInteger inputNSInt = [input integerValue];
-        NSLog(@"%@",inputNSInt);
-        
-        //canada
-        NSString *canadianized = [input stringByAppendingString:@",eh?"];
-        [NSString stringWithFormat:@"%@/%@",input,@",eh?"];
-        NSLog(@"%@", canadianized);
-        
-        //ends with ?
-        NSString *last = [input substringFromIndex:[input length]-1];
-        NSLog(@"%@", last);
-        
-        if([last isEqualToString:@"?"]){
-            NSLog(@"%@",@"I don't know");
-        }else if([last isEqualToString:@"!"]){
-            NSLog(@"%@",@"Whoa, calm down!");
-        }
-       
-        //replace splace with something else
-        NSString *space = [input stringByReplacingOccurrencesOfString:@" " withString:@"-"];
-        NSLog(@"%@",space);
-    
+    selectOptions();    
     [pool drain];
     return 0;
 
