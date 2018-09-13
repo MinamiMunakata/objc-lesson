e1e9001e873c5b1ba14bf28b224a5cd2224e7071
Minamiciccc <june28munazo@gmail.com>
2018-09-13 10:09:45 -0700

    rename project name

---
+++ Lab8-PizzaRestaurant/Lab8/Kitchen.m
@@ -0,0 +1,18 @@
+//
+//  Kitchen.m
+//  Lab8
+//
+//  Created by Steven Masuch on 2014-07-19.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import "Kitchen.h"
+
+@implementation Kitchen
+
+/*- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings
+{
+    
+}*/
+
+@end
---
+++ Lab8-PizzaRestaurant/Lab8/Pizza.h
@@ -0,0 +1,36 @@
+//
+//  Pizza.h
+//  Lab8
+//
+//  Created by Minami Munakata on 2018-09-13.
+//  Copyright © 2018 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+// Make a type with options
+// C way
+typedef enum {
+    small,
+    medium,
+    large
+} PizzaSize;
+//enum PizzaSize {
+//  small,
+//  medium,
+//  large
+//};
+
+@interface Pizza : NSObject
+
+// enum PizzaSize is now a type
+@property (nonatomic, assign) PizzaSize size;
+//@property (nonatomic, assign) enum PizzaSize size;
+@property (nonatomic, strong) NSArray *toppings;
+
+- (instancetype)initWithSize: (PizzaSize) size andToppings: (NSArray *) toppings;
++ (Pizza *) largePepperoni;
++ (Pizza *) meatLoverWithSize: (PizzaSize) size;
+
+@end
