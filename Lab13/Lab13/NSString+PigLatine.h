4fd2a7163efae7537fc5b9cd5ea5b0c610f6221e
Minamiciccc <june28munazo@gmail.com>
2018-09-17 12:44:01 -0700

    create Lab13

---
+++ Lab13/Lab13/NSString+PigLatine.h
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatine.h
+//  Lab13
+//
+//  Created by Minami Munakata on 2018-09-17.
+//  Copyright © 2018 Minami Munakata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (PigLatine)
+
+@end
---
+++ Lab13/Lab13/NSString+PigLatine.m
@@ -0,0 +1,13 @@
+//
+//  NSString+PigLatine.m
+//  Lab13
+//
+//  Created by Minami Munakata on 2018-09-17.
+//  Copyright © 2018 Minami Munakata. All rights reserved.
+//
+
+#import "NSString+PigLatine.h"
+
+@implementation NSString (PigLatine)
+
+@end
